74cd6fcc088fbdacbb9f58b7002bc9d762909137
Zinath09 <zuzabu@o2.pl>
2023-10-26 19:51:55 +0200

    solution to 2-regret

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random
+from copy import deepcopy
 def plotMap(nodes, edges=[], colors = False, cost = True):
     fig=plt.figure(figsize=(6,6), dpi= 100, facecolor='w', edgecolor='k')
     
@@ -14,7 +15,6 @@ def plotMap(nodes, edges=[], colors = False, cost = True):
     X = [c[0] for c in nodes]
     Y = [c[1] for c in nodes]
     if cost:
-        print(cost)
         S = [c[2] for c in nodes]
         plt.scatter(X, Y, c = S, cmap="grey")
     else:
@@ -35,7 +35,7 @@ def get_min_index(array):
 def Euclidian_distance(coor_1, coor_2):
     x1, y1 = coor_1
     x2, y2 = coor_2
-    return int((x2 -x1)**2 + (y2-y1)**2 )**(1/2)
+    return int(((x2 -x1)**2 + (y2-y1)**2 )**(1/2))
 
 
 def Random(cost_list, distance_matrix, lista):
@@ -114,7 +114,7 @@ def add_to_cycle(edge, new_n, dist_m,edges, cost_list):
     end_n = edge[1]
     # cost(start-end) + cost(start, new) + cost(end,new) + cost(new)
 
-    cost_diff = - dist_m[[start_n, end_n]] + dist_m[[start_n, new_n]] + dist_m[[new_n, end_n]] + cost_list[ new_n]
+    cost_diff = -dist_m[start_n][ end_n] + dist_m[start_n][ new_n] + dist_m[new_n][end_n] + cost_list[new_n]
     edges.remove(edge)
     edges.append([start_n, new_n])
     edges.append([new_n, end_n])
@@ -125,6 +125,7 @@ def cound_cost_diff_cycle(edge, new_n, dist_m, cost_list):
     end_n = edge[1]
     # cost(start-end) + cost(start, new) + cost(end,new) + cost(new)print(distance_matrix[:10][:10])
     cost_diff = -dist_m[start_n][ end_n] + dist_m[start_n][ new_n] + dist_m[new_n][end_n] + cost_list[new_n]
+
     return cost_diff
 
 def greedy_cycle(distance_matrix,cost_list,NR_NODES, HALF_NODES, starting_node ):
@@ -187,3 +188,87 @@ def present_statistic(list):
 
 def return_statistic(list):
     return min(list), max(list), np.mean(list),  np.std(list)
+
+def count_cost_diff_cycle(edge, new_n, dist_m, cost_list):
+    start_n = edge[0]
+    end_n = edge[1]
+    # cost(start-end) + cost(start, new) + cost(end,new) + cost(new)print(distance_matrix[:10][:10])
+    cost_diff = - dist_m[start_n][ end_n] + dist_m[start_n][ new_n] + dist_m[new_n][end_n] +cost_list[new_n]
+    # assert cost_diff>0, f'{cost_diff, - dist_m[start_n][ end_n], dist_m[start_n][ new_n], dist_m[new_n][end_n]}' #
+    return cost_diff
+
+def create_regret_matrix(non_visited, cur_tour, dist_m, cost_list): #cur_tour = edges
+    reg_matrix = np.zeros((len(dist_m),len(cur_tour)))
+    for new_node in non_visited:
+        for i,edge in enumerate(cur_tour):
+            reg_matrix[new_node][i]=count_cost_diff_cycle(edge, new_node, dist_m, cost_list)
+
+    return reg_matrix
+
+def return_biggest_regret(matrix):
+    min_values_for_rows = np.min(matrix, axis=1)
+    # print("min_values_for_rows",min_values_for_rows)
+    rescue_node = np.argmax(min_values_for_rows, axis=0) #najlepiej ratować 4 index
+    # print("City with bigest regret",rescue_node)
+    rescueing_node = np.argmin(matrix[rescue_node])# def return_max_from_min_rows_regret(matrix):
+    # print("Rescueing edge index to modify: ",rescueing_node)
+    return rescue_node, rescueing_node
+
+
+def two_regret_greedy_cycle(distance_matrix,cost_list,NR_NODES, HALF_NODES, starting_node, verbose = False):
+    a= starting_node
+    b = np.argmin(distance_matrix[a]+ cost_list)
+    if verbose:
+        print(a,b)
+    cur_tour = [(a,b),(b,a)]
+    non_visited = list(x for x in range(NR_NODES))
+    non_visited.remove(cur_tour[0][0])
+    non_visited.remove(cur_tour[0][1])
+    if verbose:
+        print(non_visited, cur_tour)
+    total_cost = cost_list[cur_tour[0][1]] + cost_list[cur_tour[0][0]] + 2 * distance_matrix[cur_tour[0][1]][cur_tour[0][0]]
+
+    # plotMap(data, edges=cur_tour, colors = False, cost = False)
+    # cost_matrix = start_dist_matrix * cost_list.T
+    for i in range(HALF_NODES):
+        start_dist_matrix = create_regret_matrix(non_visited, cur_tour, distance_matrix, cost_list)
+        start_dist_for_nodes = np.min(start_dist_matrix, axis = 1)
+
+        
+        max_regrets = [-1 for i in range(NR_NODES)]
+        for new_node in non_visited:
+            #zakładamy że bierzemy najlepszy edge dla danego node
+            edge = cur_tour[np.argmin(start_dist_matrix[new_node], axis=0)]
+
+            #if we add new_node in place of edge
+            sim_non_visited = deepcopy(non_visited)
+            sim_cur_tour = deepcopy(cur_tour)
+            sim_cur_tour, cost_diff = add_to_cycle(edge, new_node, distance_matrix, sim_cur_tour, cost_list)
+            sim_non_visited.remove(new_node)
+            
+            sim_cur_tour = [(new_node, edge[0]), (edge[1], new_node)]
+            
+            sim_regret_matrix = create_regret_matrix(sim_non_visited, sim_cur_tour , distance_matrix, cost_list) #[(new_node, edge[0]), (edge[1], new_node)]
+            #create sth that copy the start dist-matrix but without edge and add sim_m_regret matrix to count the min 
+            
+            first = np.min(start_dist_matrix[:], axis = 1)
+            distances_after_inserting = np.min(sim_regret_matrix, axis = 1) #min distance to every node after we insert node in particular place,
+            regret = distances_after_inserting - start_dist_for_nodes
+
+            # print(pd.DataFrame(zip(start_dist_for_nodes,distances_after_inserting, regret)))
+            max_regrets[new_node] = np.max(regret) #if we dont add this node some other node will be further by max_regret
+        # print(max_regrets)
+        added_node = np.argmax(max_regrets)
+        edge = cur_tour[np.argmin(start_dist_matrix[added_node], axis=0)]
+        
+        cur_tour, cost_diff = add_to_cycle(edge, added_node, distance_matrix, cur_tour, cost_list)
+        non_visited.remove(added_node)
+        total_cost += cost_diff
+        if verbose:
+            print(i)
+            print("COST", total_cost)
+            print("ADDED", added_node)
+            print("CUR_TOUR",cur_tour)
+    print(starting_node, total_cost)
+    return total_cost, cur_tour
+        # print("FINAL", i, total_cost)
